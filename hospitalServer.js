ab39691d539c62eba0fde52374278b7e6c488bf9
Adjei Alexander <alexanderadjei1054@gmail.com>
2023-12-17 21:27:22 -1200

    Second commit

--- hospitalServer.js
+++ hospitalServer.js
@@ -1,11 +1,27 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 
 //routes
 
+app.get('/', (req, res) => {
+    res.send('Hello API')
+})
+
+app.get('/patientRecords', (req, res) => {
+    res.send('no records yet')
+})
 
 
-app.listenerCount(3000, () => {
-    console.log('hello API')
+
+mongoose.
+connect('mongodb://localhost:27017')
+.then(() => {
+    console.log('connected to mongoDB compass')
+    app.listen(3000, () => {
+        console.log('Node API is running on port 3000')
+    })
+}).catch(() => {
+    console.log(error)
 })
---
+++ models/patientRecords.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const patientSchema = mongoose.Schema(
+    {
+        patientID: {
+            type: String,
+            required: [true, 'Please enter patientID!']
+        },
+        Surname: {
+            type:String,
+            required: [true, 'Please enter Patient_name']
+        },
+        Other_names: {
+            type: String,
+            required: [true, 'please enter patients other_names']
+        },
+        phoneNumber: {
+            type:Number,
+            required: true
+        },
+        Residential_address: {
+            type: String,
+            required: true
+        },
+        Emergency_name: {
+            type:String,
+            required: false
+        },
+        Emergency_contact:
+
+    }
+)
